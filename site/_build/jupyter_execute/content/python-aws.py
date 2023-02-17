7b98e8c0d118eaa6f896a3b3745ed48e80ea1af5
erika-tyagi <erika.tyagi@gmail.com>
2023-02-17 11:44:11 -0500

    update aws content

--- site/_build/jupyter_execute/content/python-aws.py
+++ site/_build/jupyter_execute/content/python-aws.py
@@ -3,80 +3,113 @@
 
 # # AWS & Python
 
-# From data storage to computing, Amazon Web Services (AWS) is a scalable and and cost effective alternative to on premise options. This guide demonstrates how Urban Institute researchers can use Python in conjunction with Amazon Services S3 and EC2 to create a cloud based workflow. 
+# For data storage and computing, Amazon Web Services (AWS) can be a scalable and cost-effective alternative to on-premise options. This guide demonstrates how Urban Institute researchers can use Python in conjunction with the AWS services S3 and EC2 to create a cloud-based workflow. 
 
 # ## S3
+# 
+# Boto3 is the official AWS SDK for Python. It provides an easy-to-use, well-documented [API](https://boto3.amazonaws.com/v1/documentation/api/latest/index.html) that lets you write Python code to interact with AWS services. In this guide we will explore specifically how Boto3 can be used to interact with AWS S3, Simple Storage Service. 
+# 
+# S3 can be used to store big data files for a relatively low price. It is essentially a file storage system, consisting of buckets, which are analogous to folders. You can use Boto3 to place files (referred to as objects in S3) into buckets and download objects that are stored in buckets, among other actions. In the context of a research model or script, this means you can store and read input files from S3 and write output files back to S3. 
+# 
+# ### Setup
+# Getting started with Boto3 involves installing Boto3 and authenticating using AWS credentials as described in this [guide](https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html). The most secure way to do so is by setting up your credentials using the Amazon Command Line Interface (CLI). To do so, you will need an access key and secret access key. To set up credentials with the permissions you need, you should contact the TECH team.
 
-# Boto3 is the AWS SDK for Python with an easy to use, well documented [API](https://boto3.amazonaws.com/v1/documentation/api/latest/index.html). It allows one to write Python code that interacts with AWS. In this guide we will explore specifically how Boto3 can be used to interact with AWS S3, Simple Storage Service. 
+# ### Boto3 commands
 # 
-# S3 can be used to store big data for a relatively small price. It is essentially a file storage system, consisting of buckets, which are analogous to folders. You can use Boto3 to place files into buckets and download files that are stored in buckets, among other actions. In the context of a research model or script, this means you can store and read in input files from S3 and write output files to S3. 
-# 
-# 
-# 
-# ### Setting up
+# In Boto3, there are multiple classes that enable interaction with S3. In this guide, we will use the `client` class, which is a low-level, object-oriented interface to AWS. 
 
-# Getting started with Boto3 involves installing Boto3 and authenticating yourself using your AWS credentials as described in this quickstart [guide](https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html). The most secure way to do so is by setting up your credentials file using the Amazon Command Line Interface (CLI). In order to do this, you will need your own access key and secret access key. In order to set up a role with the permissions you need, you should contact the tech team.
-
-# ### Running Boto3 Commands
-
-# Using Boto3, there are multiple classes that will allow interaction with S3. In this guide we will use the `resource` class, which is a high level, object oriented interface to AWS. 
+# #### Listing buckets and objects
 # 
-# First, we will instantiate a service resource which represents the Amazon S3 service. 
+# To list buckets and objects in S3, you first you need to instantiate a client resource which represents the Amazon S3 service. 
 # 
-# In order to view all of the buckets that are associated with your AWS S3 account, you can run the below code. If your desired bucket is not there, contact the tech team to adjust the permissions associated with you account.
+# Then, you can use the syntax below to list all of the buckets that you have access to. If your desired bucket doesn't show up, contact the TECH team to update the permissions associated with your credentials.
 # 
 # ```
 # import boto3
-# s3 = boto3.resource('s3')
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
+# s3 = boto3.client("s3")
+# 
+# response = s3.list_buckets()
+# for bucket in response["Buckets"]:
+#     print(f'{bucket["Name"]}')
 # ```
 
-# Next you can instantiate a bucket resource representing your desired bucket. This will allow you to interact with the bucket. The code below demonstrates using an instantiated bucket to download a file from S3:
+# Note that S3 uses key-value pairs to store objects, rather than a traditional file system with a hierarchy of folders. You can use [prefixes](https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-prefixes.html) to organize files in S3 buckets, which are conceptually similar to directories or subfolders. The code below would list the name of all objects within a particular prefix that you have access to. 
 # 
-# ```
-# mybucket = s3.Bucket('mybucket')
-# mybucket.download_file('example.txt', '/tmp/example.txt')
-# ```
+# ``` 
+# response = s3.list_objects_v2(Bucket="my-bucket",
+#                               Prefix="my-prefix")
 # 
-# And upload a file to S3:
-# 
-# ```
-# mybucket.upload_file('/tmp/example.txt', 'example.txt')
+# for object in response["Contents"]: 
+#     print(f'{object["Key"]}')
 # ```
 
-# For a full list of operations you can perform on your bucket, refer to this [documentation]( https://boto3.amazonaws.com/v1/documentation/api/1.9.96/reference/services/s3.html#bucket). Keep in mind that your  will need to have specific permission associated with your account to perform any action.
+# #### Uploading and downloading files
 # 
-# ### S3 Triggers
+# Boto3 also provides simple methods to upload and download files from S3. The code below would upload a file stored locally called `example.csv` to the specified bucket and prefix. 
+# 
+# ```
+# s3.upload_file(Bucket="my-bucket", 
+#                Filename="example.csv", 
+#                Key="my-prefix/example.csv")
+# ```
+# 
+# Similarly, the code below would download that file from S3. 
+# 
+# ```
+# s3.download_file(Bucket="my-bucket", 
+#                  Filename="example.csv", 
+#                  Key="my-prefix/example.csv")
+# ```
+# 
+# For a full list of operations you can perform on your bucket using Boto3, refer to this [documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#client). Keep in mind that you will need to have specific permissions associated with your credentials to perform any action.
+
+# #### Using Pandas
+# Recent versions of Pandas let you read and write files to S3 with simplified syntax using the s3fs package under-the-hood. You can install s3fs using `pip install s3fs`. 
+# 
+# The code below would read the file `example.csv` from S3 as a Pandas dataframe. 
+# 
+# ```
+# import pandas as pd 
+# df = pd.read_csv("s3://my-bucket/my-prefix/example.csv")
+# ```
+# 
+# Similarly, the code below would write that dataframe to S3 at the specified path. 
+# ```
+# df.to_csv("s3://my-bucket/my-prefix/example.csv")
+# ```
+
+# 
+# ### S3 triggers
 # S3 can be used to trigger AWS events relating to other services. For example an S3 bucket can be configured so that when a file is placed into it:
 # * An email is sent to a user
 # * An EC2 instance spins up and runs a script which takes in that file as input
 # 
-# Contact the tech team if you would like to set up a trigger for your S3 bucket. 
+# Contact the TECH team if you would like to set up a trigger for your S3 bucket. 
 
 # ## EC2
 
-# EC2 is an Amazon service which provides compute capacity in the cloud. Rather than running your models and scripts on Urban on premise servers, you can run them on EC2 which provides customizable compute capacity, memory, and more. 
+# EC2, or Elastic Compute Cloud, is an Amazon service which provides virtual computers in the cloud. Rather than running your models and scripts on your local computer or on Urban servers, you can run them on EC2 which provides customizable compute capacity, memory, and more.  
 # 
-# ### Using tech-tools to spin up an EC2 instance
+# ### Using Tech Tools to spin up an EC2 instance
 # 
-# The tech team at Urban has designed a tool for researchers to easily create and terminate EC2 instances. This tool is hosted on the tech-tools site via the Urban Institute intranet page. 
+# The TECH team at Urban has designed a tool for researchers to easily create and terminate EC2 instances. This tool is hosted on the Tech Tools site via the Urban Institute intranet [here](https://tech-tools.urban.org/ec2-submission/).
 # 
-# To begin, under the "Research Platforms" dropdown on the tech-tools site, select "Start or Stop EC2 instance." In order to spin up your EC2 instance, you will be able to select which instance type you would like based on your specific needs and submit the form. 
+# **Confidential Data**: These instances are NOT suitable for processing confidential data, but if you are working on a project that uses confidential data and you would like to leverage an EC2 instance, the TECH team would be happy to create a custom instance that adheres to data use requirements for you. 
+# 
+# The Python interface for the Tech Tools instances use the latest `jupyter/scipy-notebook` [Docker image](https://jupyter-docker-stacks.readthedocs.io/en/latest/using/selecting.html#jupyter-scipy-notebook). This means that a recent version of Python and many commonly used packages for data analysis and scientific computing will come pre-installed. This also means that you will get access to a user-friendly interface for creating and working with Jupyter notebooks, text editors, and terminal windows. See the [JupyterLab documentation](https://jupyterlab.readthedocs.io/en/stable/index.html) for more info. 
 
 # ![title](img/tech-tools.png)
 
-# After your instance has spun up, you will receive an email which contains links that point your browser to Jupyter or RStudio on your instance. Note: You must be within Urban's IP range to access those links from your browser (e.g. through a virtual desktop if you are working remotely). 
+# To begin, under the “Research Platforms” dropdown on the Tech Tools site, select “Start or Stop EC2 instance.” Then, select the instance type you would like based on your specific needs and submit the form. For Python users, we generally recommend Linux instances. 
 # 
-# You can also use your instance's IP to SSH into it. When you are finished with the instance, you can terminate it, and you will be charged only for the amount of time that it was running. 
-# 
+# After your instance has spun up, you will receive an email with links that point your browser to JupyterLab or RStudio on your instance. Note: You must be within Urban's IP range to access those links from your browser (e.g. through a virtual desktop if you are working remotely). When you are finished with the instance, you can terminate it, and you will be charged only for the amount of time that it was running. You will also get periodic email reminders while your instance is running. 
 
 # ## Putting it all together
 
-# S3 and EC2 can be used to create a workflow entirely hosted in the cloud. The tech-tools site simplifies the process of working with EC2, abstracting many details of the creation and termination away from the user. In terms of more complex workflows using EC2 and other services, the tech team can partner with your team for design and implementation. in order to create more complex workflows. An example workflow for a Python data processing script is detailed below.
+# S3 and EC2 can be used to create a workflow entirely hosted in the cloud. The Tech Tools site simplifies the process of working with EC2, abstracting many details of the creation and termination away from the user. For more complex workflows using EC2 and other AWS services, the TECH team can partner with you to design and implement a solution. An example workflow for a Python data processing script is detailed below. 
 # 
-# As mentioned earlier, a rule can be set so that the placement of a file into an S3 bucket triggers the creation of an EC2 instance. This workflow begins with an input data file being placed into an input S3 bucket via Boto3. This triggers an EC2 instance to spin up, obtain the input file, and run the data processing script. At the end of the run, the output file is written to the output S3 bucket, and the EC2 instance automatically terminates. Additionally, a rule is set on the output bucket to trigger a notification of the user when a file is placed into it.
+# As mentioned earlier, a rule can be set so that the placement of a file into an S3 bucket triggers the creation of an EC2 instance. This workflow begins with an input data file being placed into an input S3 bucket via Boto3. This triggers an EC2 instance to spin up, obtain the input file, and run the data processing script. When the script completes, the output file is written to the output S3 bucket, and the EC2 instance automatically terminates. Additionally, a rule is set on the output bucket to trigger a notification of the user when a file is placed into it.
 
 # ![title](img/aws-workflow.png)
 
-# This and similar workflows have been employed by the Urban Institute tech team to run data processing scripts and research models, including the TPC Tax Calculator and the Social Genome Model. Both of the models use Boto3 to place and retrieve objects in S3, and the Social Genome Model leverages Python's compatibility with EC2.
+# This and similar workflows have been employed by the Urban Institute TECH team to run data processing scripts and research models, including the TPC Tax Calculator, the Education Data Portal, and the Social Genome Model. 
